c8f1c501f85dae2ae16377f07a430cef61a936e4
Jake Stride <jake.stride@senokian.com>
2006-01-11 15:15:06 +0000

    Added description and license header.

--- modules/admin/announcements.php
+++ modules/admin/announcements.php
@@ -1,4 +1,35 @@
 <?php
+// +----------------------------------------------------------------------+
+// | Enterprise Groupware System (EGS) - Announcements Overview 1.0       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2002-2006 Senokian Solutions                           |
+// +----------------------------------------------------------------------+
+// | This file is part of EGS.                                            |
+// |                                                                      |
+// | EGS is free software; you can redistribute it and/or modify it under |
+// | the terms of the GNU General Public License as published by the Free |
+// | Software Foundation; either version 2 of the License, or (at your    |
+// | option) any later version.                                           |
+// |                                                                      |
+// | EGS is distributed in the hope that it will be useful, but WITHOUT   |
+// | ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   |
+// | or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public     |
+// | License for more details.                                            |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with EGS; if not, write to the Free Software Foundation, Inc., |
+// |
+// | 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA               |
+// +----------------------------------------------------------------------+
+// | Author: Jake Stride <jake.stride@senokian.com>                       |
+// +----------------------------------------------------------------------+
+// | Changes:                                                             |
+// |                                                                      |
+// | 1.0                                                                  |
+// | ===                                                                  |
+// | Initial Stable Release                                               |
+// +----------------------------------------------------------------------+
+//
 if (in_array('admin', $_SESSION['modules'])) {
 	/* If the page has not been set, set it */
 	if(isset($_GET['page'])) $_SESSION['adminnews_page'] = max(1, intval($_GET['page']));
